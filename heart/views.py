c4e0b0430f3ca360326778541b50b6ee55b93724
npaudyal321 <naween321@gmail.com>
2022-12-24 21:52:43 +0545

    view to image classifier with file upload and context

--- heart/views.py
+++ heart/views.py
@@ -1,6 +1,23 @@
+import base64
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from heart.utils import classify_image as clssfy_image, load_saved_artifacts
+
 
 def classify_image(request):
+    if request.method == 'POST':
+        context = {"name": "Please add picture"}
+        file = request.FILES.get("file")
+        if file:
+            byte_code = file.read()
+            en = base64.b64encode(byte_code)
+            d = en.decode("utf-8")
+            load_saved_artifacts()
+            p = clssfy_image(en, None)
+            if p:
+                context = {"name": p[0]['class'].upper()}
+            else:
+                context = {"name": "Can't Recognize"}
+        return render(request, 'classify.html', context=context)
     return render(request, 'classify.html')
