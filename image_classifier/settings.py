1e13338d96b73749ea7caf26c3a9fa216ba95682
npaudyal321 <naween321@gmail.com>
2023-01-16 22:14:58 +0545

    update allowed hosts

--- image_classifier/settings.py
+++ image_classifier/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = DEBUG
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ALLOWED_HOSTS
 
 
 # Application definition
